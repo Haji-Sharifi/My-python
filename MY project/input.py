9df0ecffb03c9a7a53bd6c535b90237cc7119c97
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-11 14:40:57 +0430

    m

---
+++ MY project/input.py
@@ -0,0 +1,9 @@
+# input() = A function that prompts the user to enter data
+#           Returns the enterd data as String
+
+name = input("What is you name?:")
+age = input("how old are you?:")
+
+print(f"Hello {name}!")
+print(f"you are {age} old")
+
