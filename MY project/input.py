5bc7d7c6e4d39d35100588fc1dd61fbcf4038152
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-13 20:05:38 +0430

    add

--- MY project/input.py
+++ MY project/input.py
@@ -1,7 +1,7 @@
 # input() = A function that prompts the user to enter data
 #           Returns the enterd data as String
 
-name = input("What is you name?:")
+# name = input("What is you name?:")
 # age = int(input("how old are you?:"))
 
 
@@ -9,14 +9,28 @@ name = input("What is you name?:")
 # age = age + 1
 
 
-print(f"Hello {name}!")
-print("HAPPY BIRTHDAY!")
+# print(f"Hello {name}!")
+# print("HAPPY BIRTHDAY!")
 # print(f"you are {age} old")
 
 # Exercise 1 Rectangle Area Clac 
 
-lenght = float(input("Enter the lenght:"))
-width = float(input("Enter the width:"))
-area = width * lenght 
+# lenght = float(input("Enter the lenght:"))
+# width = float(input("Enter the width:"))
+# area = width * lenght 
 
-print(area)
+# print(f"the area is: {area} cm²")
+
+# to use the power of two you need to turn on num lock
+# then press alt key and enter 0178 
+
+
+# Shoping Cart program
+
+item = input("what item would you like to buy?")
+price = float(input("what is the price?: "))
+quantity = int(input("how many would you like ? "))
+
+total = price * quantity
+
+print(f"you have bought {quantity} X {total}")
