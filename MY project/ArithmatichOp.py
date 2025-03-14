15c931e13f86dc9ee9122d0733c0d92dbf278730
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-14 20:13:54 +0430

    ADD

--- MY project/ArithmatichOp.py
+++ MY project/ArithmatichOp.py
@@ -1,4 +1,4 @@
-friends = 10
+# friends = 10
 
 # friends = friends + 1
 # friends += 1 
@@ -15,7 +15,69 @@ friends = 10
 # friends = friends ** 2 
 # friends **= 2 
 
-reminder = friends % 2
+# reminder = friends % 2
 
-print(reminder)
+# print(reminder)
 # print(friends)
+
+# x = 3.14
+# y= 4
+# z= 5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = min(x, y , z)
+# result = max(x, y , z)
+
+# print(result)
+
+
+
+
+
+        # math functions
+# import math
+
+
+
+# x = 9.5
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(x)
+# result = math.ceil(x)  # ceil is used for rounding up
+# result = math.floor(x) # floor is used for rounding down
+
+# print(result)
+
+    # exersices 
+
+
+# import math 
+
+# radius = float(input('Enter a radius a circle: '))
+
+# circumfrence = 2 * math.pi * radius
+
+# print(f"the circumfernece is : {round(circumfrence, 2)} cm.")
+
+
+# import math
+
+# radius = float(input('Enter a radius of a circle: '))
+
+# area = math.pi * pow(radius, 2)
+
+# print(f'The area of the circle is : {round(area , 2)} cm^2')
+
+
+import math
+a = float(input('Enter side A: '))
+b = float(input('Enter side B: '))
+
+c = math.sqrt(pow(a, 2) + pow(b, 2))
+
+print(f'side C = {c}')
+
+
