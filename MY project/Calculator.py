4f947ae7864caa8668c4df5fc22815d24804eca3
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-16 22:35:09 +0430

    Calculator

--- MY project/Calculator.py
+++ MY project/Calculator.py
@@ -7,13 +7,15 @@ num2 = float(input("Enter the 2nd number: "))
 
 if operator == "+":
     result = num1 + num2
-    print(round(result))
+    print(round(result , 3))
 elif operator == "-":
     result = num1 - num2
-    print(round(result))
+    print(round(result , 3))
 elif operator == "*":
     result = num1 * num2
-    print(round(result))
+    print(round(result , 3))
 elif operator == "/":
     result = num1 / num2
-    print(round(result))
+    print(round(result , 3))
+else:
+    print(f"{operator} is not a valid operator")
