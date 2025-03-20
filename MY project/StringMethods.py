4f9e95ebdff8bb81972c5d06c1af2b56c964677b
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-20 21:52:58 +0430

    M

---
+++ MY project/StringMethods.py
@@ -0,0 +1,43 @@
+
+
+# name = input("Enter your full name : ")
+# phoneNO = input("Enter your phone # : " )
+
+# result = len(name)
+# result = name.find(" ")
+# result = name.rfind("o")
+# result = name.capitalize()
+# result = name.upper()
+# result = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+
+# result = phoneNO.count("-")
+# result = phoneNO.replace("-" , " ")
+
+# print(help(str)) # by using this line code you can get help through the string methods 
+
+# print(result)
+
+# validate user input exercise 
+# 1. username is no more than 12 characters
+# 2. username must not contain spaces
+# 3. username must not contain digits
+
+# task num 1
+
+username = input("Enter a username : ")
+
+if len(username) > 12 :
+    print("your username can't be more than 12 characters")
+elif not username.find(" ") == -1 :
+    print("Your user name can not contian spaces")
+elif not username.isalpha() :
+    print("Your username con't contian number ")
+else:
+    print(f"Welcome {username}")
+
+
+
+
+
