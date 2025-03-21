360a38dd7a88e1a68272b180ba8a3260f352e0f9
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-21 19:07:02 +0430

    ::

--- MY project/Indexing.py
+++ MY project/Indexing.py
@@ -1,6 +1,8 @@
 # indexging = accessing elements of a sequence using [] (indexing operation)
 #             [start : end : step]
 
-creditNumber = "123-456-789-0123"
+creditNumber = "1234-5678-9012-3456"
 
-print(creditNumber[0])
+# print(creditNumber[0])
+
+print(creditNumber[0 : 4])
