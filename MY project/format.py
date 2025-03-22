25474ecf86bc581b7a5fc474520b43f0c679d206
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-22 14:34:18 +0430

    Add

--- MY project/format.py
+++ MY project/format.py
@@ -5,3 +5,20 @@
 # :(number) = allocate that many spaces
 # :03 = allocate that many spaces
 # :< = left justify
+# :^ = center align
+# :+ = use a plus sign to indicate positive value 
+# := = place sign to leftmost position
+# :  = insert a space before positive numbers
+# :, = comma separator
+
+
+price1 = 3.14159
+price2 = -987.65
+price3 = 12.34
+
+
+print(f"price 1 is {price1:.2f}")  #showing decimal point
+print(f"price 2 is {price2}")
+print(f"price 3 is {price3}")
+
+      
