a1943b1d1958573a6e6316a66e24aea5a506e475
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-22 22:41:55 +0430

    M

--- MY project/format.py
+++ MY project/format.py
@@ -17,6 +17,6 @@ price2 = -987.65
 price3 = 12.34
 
 
-print(f"price 1 is ${price1:.1f}")  #showing decimal point
-print(f"price 2 is ${price2:.1f}")
-print(f"price 3 is ${price3:.1f}")
+print(f"price 1 is ${price1:<10}")  #showing decimal point
+print(f"price 2 is ${price2:<10}")
+print(f"price 3 is ${price3:<10}")
