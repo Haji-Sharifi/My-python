7a01b3582238ae7ddfa83fe74c349c3ec9b53773
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-22 14:25:56 +0430

    Mm

---
+++ MY project/format.py
@@ -0,0 +1,7 @@
+# format specifiers = {:flags} format a value based on what 
+#                              flage are inserted
+
+# .(number)f = round to that many decimal places (fixed point)
+# :(number) = allocate that many spaces
+# :03 = allocate that many spaces
+# :< = left justify
