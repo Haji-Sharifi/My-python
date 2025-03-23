d4520bcae9e7b06c059717f94cf2f3696629b302
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-23 13:01:07 +0430

    ,

--- MY project/While.py
+++ MY project/While.py
@@ -17,10 +17,19 @@
 # print(f"Your age is {age}")
 
 
-food = input("Enter your favorite food : (q to quit) ")
+# food = input("Enter your favorite food : (q to quit) ")
 
-while not food == "q":
-    print(f"you like {food}")
-    food = input("Enter your favorite food : (q to quit) ")
+# while not food == "q":
+#     print(f"you like {food}")
+#     food = input("Enter your favorite food : (q to quit) ")
 
-print("Goodbye")
+# print("Goodbye")
+
+num = int(input("Enter a number between 1 - 10: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("Enter a number between 1 - 10: "))
+    
+print(f"You entered {num}")
+
