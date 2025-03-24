a73064f9d1b2622b5c44c610a2ee7b6386c75fea
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-24 11:53:11 +0430

    My-Calculator

--- MY project/intrestCalculator.py
+++ MY project/intrestCalculator.py
@@ -1,23 +1,54 @@
 # Python Compound intrest claculator
 
+# principle = 0
+# rate = 0
+# time = 0
+
+# while principle <= 0:
+#     principle = float(input("Enter the principle amount : "))
+#     if principle <= 0:
+#         print("Principle amount can not be negative or zero")
+
+# while rate <= 0:
+#     rate = float(input("Enter the rate of intrest : "))
+#     if rate <= 0:
+#         print("Rate of intrest can not be negative or zero")
+# while time <= 0:
+#     time = float(input("Enter the time in years : "))
+#     if time <= 0:
+#         print("Time can not be negative or zero")
+
+
+# total = principle * pow((1 + rate / 100) , time)
+# print(f"Balance after {time} years is : ${total:.2f}")
+
+
 principle = 0
 rate = 0
 time = 0
 
-while principle <= 0:
-    principle = float(input("Enter the principle amount : "))
-    if principle <= 0:
-        print("Principle amount can not be negative or zero")
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle == 0:
+        print("Principle amount cannot be zero.")
+    else:
+        break
 
-while rate <= 0:
-    rate = float(input("Enter the rate of intrest : "))
-    if rate <= 0:
-        print("Rate of intrest can not be negative or zero")
-while time <= 0:
-    time = float(input("Enter the time in years : "))
-    if time <= 0:
-        print("Time can not be negative or zero")
+while True:
+    rate = float(input("Enter the rate of interest: "))
+    if rate == 0:
+        print("Rate of interest cannot be zero.")
+    else:
+        break
+
+while True:
+    time = float(input("Enter the time in years: "))
+    if time == 0:
+        print("Time cannot be zero.")
+    else:
+        break
 
 
-total = principle * pow((1 + rate / 100) , time)
-print(f"Balance after {time} years is : ${total:.2f}")
+total = principle * pow((1 + rate / 100), time)
+
+print(f"Balance after {time} years is: ${total:.2f}")
