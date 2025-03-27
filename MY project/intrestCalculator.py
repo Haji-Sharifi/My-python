a509d251649de54b56be3f95bbc50f4ec7fe2f21
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-27 19:36:10 +0430

    m

--- MY project/intrestCalculator.py
+++ MY project/intrestCalculator.py
@@ -23,32 +23,32 @@
 # print(f"Balance after {time} years is : ${total:.2f}")
 
 
-# principle = 0
-# rate = 0
-# time = 0
+principle = 0
+rate = 0
+time = 0
 
-# while True:
-#     principle = float(input("Enter the principle amount: "))
-#     if principle == 0:
-#         print("Principle amount cannot be zero.")
-#     else:
-#         break
+while True:
+    principle = float(input("Enter the principle amount: "))
+    if principle == 0:
+        print("Principle amount cannot be zero.")
+    else:
+        break
 
-# while True:
-#     rate = float(input("Enter the rate of interest: "))
-#     if rate == 0:
-#         print("Rate of interest cannot be zero.")
-#     else:
-#         break
+while True:
+    rate = float(input("Enter the rate of interest: "))
+    if rate == 0:
+        print("Rate of interest cannot be zero.")
+    else:
+        break
 
-# while True:
-#     time = float(input("Enter the time in years: "))
-#     if time == 0:
-#         print("Time cannot be zero.")
-#     else:
-#         break
+while True:
+    time = float(input("Enter the time in years: "))
+    if time == 0:
+        print("Time cannot be zero.")
+    else:
+        break
 
 
-# total = principle * pow((1 + rate / 100), time)
+total = principle * pow((1 + rate / 100), time)
 
-# print(f"Balance after {time} years is: ${total:.2f}")
+print(f"Balance after {time} years is: ${total:.2f}")
