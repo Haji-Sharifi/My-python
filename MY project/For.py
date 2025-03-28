5ef77d50c9beb7a9c8350390916bd81d84e03426
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-28 13:33:03 +0430

    For LOOP

--- MY project/For.py
+++ MY project/For.py
@@ -17,5 +17,11 @@
 # it will print the numbers from 10 to 1
 # The reversed function reverses the order of the sequence
 
-for x in range(1, 11 ,2) :
-    print(x)
+# for x in range(1, 11 ,2) :
+#     print(x)
+# it will print the odd numbers from 1 to 10
+
+creditCard = "1234-5678-9012-3456"
+for x in creditCard:
+    print(x)    # this will loop over the string 
+
