fbd5b23f04e542345a96446f0a0de8685c141558
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-03-28 13:22:10 +0430

    FOR

---
+++ MY project/For.py
@@ -0,0 +1,8 @@
+# For loops = execute a block of code a fixed number of times 
+#             You can iterate over a range, string, sequence, etc.
+# Syntax: for x in range(0,10):
+#                 print(x)
+
+
+
+
