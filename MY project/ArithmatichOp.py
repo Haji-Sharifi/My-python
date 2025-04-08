f695db5a506babf4274d68b6d9b9e4e5953bd92c
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-08 22:02:11 +0430

    added

--- MY project/ArithmatichOp.py
+++ MY project/ArithmatichOp.py
@@ -72,12 +72,12 @@
 # print(f'The area of the circle is : {round(area , 2)} cm^2')
 
 
-# import math
-# a = float(input('Enter side A: '))
-# b = float(input('Enter side B: '))
+import math
+a = float(input('Enter side A: '))
+b = float(input('Enter side B: '))
 
-# c = math.sqrt(pow(a, 2) + pow(b, 2))
+c = math.sqrt(pow(a, 2) + pow(b, 2))
 
-# print(f'side C = {c}')
+print(f'side C = {c}')
 
 
