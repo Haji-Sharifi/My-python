fc932c5f7eefcbf038455eb89a6db3b04e7e9bd0
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-09 21:33:58 +0430

    update

--- MY project/CountDown.py
+++ MY project/CountDown.py
@@ -9,11 +9,20 @@
 # print("Time's up!")
 
 
+# import time 
+
+# myTime = int(input("Enter the time in seconds: "))
+
+# for x in reversed(range(0, myTime)):
+#     print(x)
+#     time.sleep(1)
+# print("Time's up!")
+
 import time 
 
 myTime = int(input("Enter the time in seconds: "))
 
-for x in reversed(range(0, myTime)):
+for x in range(myTime, 0, -1):
     print(x)
     time.sleep(1)
 print("Time's up!")
