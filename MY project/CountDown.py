f57fcc83c42db14c900e38bc6a2a6e88186049fb
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-11 23:04:31 +0430

    Updated

--- MY project/CountDown.py
+++ MY project/CountDown.py
@@ -25,6 +25,7 @@ myTime = int(input("Enter the time in seconds: "))
 for x in range(myTime, 0, -1):
     seconds = x % 60 
     minutes = int(x / 60 ) % 60 
-    print(f"00:{minutes:02}:{seconds:02}")
+    hours = int(x / 3600) % 24
+    print(f"{hours}:02{minutes:02}:{seconds:02}")
     time.sleep(1)
 print("Time's up!")
