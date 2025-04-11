4af7c8a7fc12f2869a3cdd179d321248ac6bea0a
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-11 23:15:23 +0430

    n

--- MY project/nestedLoop.py
+++ MY project/nestedLoop.py
@@ -1,3 +1,8 @@
 # nested loop = A loop within another loop (outer , inner)
 #               outer looop :
-#               inner loop : 
+#                    inner loop : 
+
+
+for i in range(3):
+for i in range(1, 10) :
+    print(i, end=' ')
