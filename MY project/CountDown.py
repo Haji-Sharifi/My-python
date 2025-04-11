489101e8a291dd7a8c95bf4e78352a5287ec57bd
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-11 22:51:23 +0430

    Added

--- MY project/CountDown.py
+++ MY project/CountDown.py
@@ -24,6 +24,6 @@ myTime = int(input("Enter the time in seconds: "))
 
 for x in range(myTime, 0, -1):
     seconds = x % 60 
-    print(x)
+    print(f"00:00:{seconds}")
     time.sleep(1)
 print("Time's up!")
