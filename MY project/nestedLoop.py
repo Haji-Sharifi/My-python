7bb41f167b21497d16d67c03344d8ce6548ae441
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-12 21:50:27 +0430

    update

--- MY project/nestedLoop.py
+++ MY project/nestedLoop.py
@@ -4,8 +4,9 @@
 
 rows = int(input("Enter the # of rows: "))
 columns = int(input("Enter the # of column: "))
+symbol = input("Enter the symbol you want to use: ")
 
-for i in range(3):
-    for j in range(1, 10) :
-        print(j, end=' ')
+for i in range(rows):
+    for j in range(columns) :
+        print(symbol, end=' ')
     print()
