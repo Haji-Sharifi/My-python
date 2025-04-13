13f2cad4edbc5778169ed4a36765719271db5002
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-13 21:27:43 +0430

    git

--- MY project/collectioVar.py
+++ MY project/collectioVar.py
@@ -5,4 +5,11 @@
 
 fruits = ['apple', 'banana', 'cherry']
 
-print(fruits)
+print(fruits[1])  # Output: banana
+print(fruits[-1])  # Output: cherry
+print(fruits[1:3])  # Output: ['banana', 'cherry']
+print(fruits[1:])  # Output: ['banana', 'cherry']
+print(fruits[:2])  # Output: ['apple', 'banana']
+print(fruits[-2:])  # Output: ['banana', 'cherry']
+print(fruits[-3:-1])  # Output: ['apple', 'banana']
+print(fruits[-2:])  # Output: ['banana', 'cherry']   
