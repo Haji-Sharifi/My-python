14ba054410ce7b734dd6bed5e9ddab87b866c7fd
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-13 21:25:31 +0430

    git

--- MY project/collectioVar.py
+++ MY project/collectioVar.py
@@ -1,4 +1,8 @@
 # collection = single 'variable' used to store multiple values 
 #   List     = []   - ordered and changeable. Duplicates OK
-#   set      = {}   - unordered and unchangeable. No duplicates
+#   set      = {}   - unordered and immutable, but Add/Remove OK. No duplicates
 #  Tuple     = ()   - ordered and unchangeable. Duplicates OK Faster 
+
+fruits = ['apple', 'banana', 'cherry']
+
+print(fruits)
