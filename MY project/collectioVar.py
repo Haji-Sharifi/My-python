2c869421298c8f552d9351c9320c2bee5cd74e39
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-17 19:36:15 +0430

    M

--- MY project/collectioVar.py
+++ MY project/collectioVar.py
@@ -3,11 +3,12 @@
 #   set      = {}   - unordered and immutable, but Add/Remove OK. No duplicates
 #  Tuple     = ()   - ordered and unchangeable. Duplicates OK Faster 
 
-fruits = ['apple', 'banana', 'cherry']
-print(dir(fruits))
-print(help(fruits))
+# fruits = ['apple', 'banana', 'cherry']
+# print(dir(fruits))
+# print(help(fruits))
 
 
 # print(dir(fruits))
 # for frut in fruits:
 #    print(frut)
+
