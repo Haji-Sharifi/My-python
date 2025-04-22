4b1670a93b4e40baf687783d84985689f6649a9a
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-22 21:52:26 +0430

    update

--- MY project/collectioVar.py
+++ MY project/collectioVar.py
@@ -31,8 +31,16 @@
 
 #  using set {}
 
-fruits = {'apple', 'banana', 'cherry', 'orange', }
+# fruits = {'apple', 'banana', 'cherry', 'orange', }
 
 # print('pineapple' in fruits) # False
 
-print(fruits[0]) # TypeError: 'set' object is not subscriptable
+# print(fruits[0]) # TypeError: 'set' object is not subscriptable
+
+
+
+# using Tuple 
+
+fruits = ('apple', 'banana', 'cherry', 'orange', )
+
+print(fruits)
