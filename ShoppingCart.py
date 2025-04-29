56798e850ac3ae1950e31693570dba882c2ab6c5
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-04-29 22:20:29 +0430

    added;

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,1 +1,14 @@
 # Shopping cart program
+
+food = []
+prices = []
+total = 0
+while True:
+    food = input("Enter the food item (or 'done' to finish): ")
+    if food.lower() == 'done':
+        break
+    else :
+        price = float(input(f"Enter the price of {food}: "))
+        prices.append(price)
+        total += price
+        print(f"{food} added to cart. Current total: ${total:.2f}")
