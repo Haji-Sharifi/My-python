c1be7ff0cd4a00c056ac94365f3597bc2fe677d0
Haji-Sharifi <mustafasharifi53@icloud.com>
2025-05-18 20:38:22 +0430

    update

--- 2Dlist.py
+++ 2Dlist.py
@@ -1,10 +1,11 @@
-fruits = ["apple", "orange", "banana", "coconut"]
+fruits =     ["apple", "orange", "banana", "coconut"]
 vegtabeles = ["celery", "carrots", "poraroes"]
-meats = ["checken" , "fish" , "turkey"]
+meats =      ["checken" , "fish" , "turkey"]
 
-groceries = [fruits, vegtabeles, meats]
+groceries =  [fruits, vegtabeles, meats]
 
 # fruits[0] = "kiwi"
 # print(fruits)
 
-print(groceries)
+print(groceries[0][0])
+print(groceries[1][2])
